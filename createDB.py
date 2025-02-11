0f52b5af819fa25ed70a20238d074ff468071e98
AlexButuc <alexandrusbutuc@gmail.com>
2025-02-11 10:44:30 +0000

    Updated createdb hostip

--- createDB.py
+++ createDB.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="database-1.crosc2qqg17q.eu-north-1.rds.amazonaws.com",
+  host="database-1.cv4842ms2b5l.eu-north-1.rds.amazonaws.com",
   user="admin",
   password="P*B&a33v+!7xj+*"
 )
